b543576d8f49d01ba49b486694809012ffc83b27
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-08-28 17:05:20 +0530

    Create ecsUtils.groovy

---
+++ vars/ecsUtils.groovy
@@ -0,0 +1,26 @@
+def call(String imageUri) {
+    def taskDefinition = """
+    {
+        "family": "nginx-task",
+        "networkMode": "awsvpc",
+        "requiresCompatibilities": ["FARGATE"],
+        "executionRoleArn": "arn:aws:iam::123456789012:role/ecsTaskExecutionRole",
+        "containerDefinitions": [
+            {
+                "name": "nginx-container",
+                "image": "${imageUri}",
+                "memory": "512",
+                "cpu": "256",
+                "essential": true,
+                "portMappings": [
+                    {
+                        "containerPort": 80,
+                        "hostPort": 80
+                    }
+                ]
+            }
+        ]
+    }
+    """
+    return taskDefinition
+}
