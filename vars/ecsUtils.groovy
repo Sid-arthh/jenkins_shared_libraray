9b32a1e6d23d1400292b60d79cb2f5578ba4b9e3
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-08-29 11:39:07 +0530

    Update ecsUtils.groovy

--- vars/ecsUtils.groovy
+++ vars/ecsUtils.groovy
@@ -1,26 +1,26 @@
 def call(String imageUri) {
     def taskDefinition = """
     {
-        "family": "nginx-task",
-        "networkMode": "awsvpc",
-        "requiresCompatibilities": ["FARGATE"],
-        "executionRoleArn": "arn:aws:iam::123456789012:role/ecsTaskExecutionRole",
-        "containerDefinitions": [
-            {
-                "name": "nginx-container",
-                "image": "${imageUri}",
-                "memory": "512",
-                "cpu": "256",
-                "essential": true,
-                "portMappings": [
-                    {
-                        "containerPort": 80,
-                        "hostPort": 80
-                    }
-                ]
-            }
-        ]
-    }
+    "family": "nginx-task",
+    "networkMode": "awsvpc",
+    "requiresCompatibilities": ["FARGATE"],
+    "executionRoleArn": "arn:aws:iam::123456789012:role/ecsTaskExecutionRole",
+    "containerDefinitions": [
+        {
+            "name": "nginx-container",
+            "image": "public.ecr.aws/nginx/nginx:mainline-alpine",
+            "memory": 512,
+            "cpu": 256,
+            "essential": true,
+            "portMappings": [
+                {
+                    "containerPort": 80,
+                    "hostPort": 80
+                }
+            ]
+        }
+    ]
+}
     """
     return taskDefinition
 }
