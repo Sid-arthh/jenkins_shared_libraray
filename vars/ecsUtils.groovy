3c4d13c2d2f35935c7b99bc16f46e86c33052b8d
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-08-29 11:50:57 +0530

    Update ecsUtils.groovy

--- vars/ecsUtils.groovy
+++ vars/ecsUtils.groovy
@@ -5,12 +5,13 @@ def call(String imageUri) {
     "networkMode": "awsvpc",
     "requiresCompatibilities": ["FARGATE"],
     "executionRoleArn": "arn:aws:iam::491396807599:role/ecsTaskExecutionRole",
+    "cpu": "256",  // Define the cpu value at the task level
+    "memory": "512", // Define the memory value at the task level
     "containerDefinitions": [
         {
             "name": "nginx-container",
             "image": "'${imageUri}'",
             "memory": 512,
-            "cpu": 256,
             "essential": true,
             "portMappings": [
                 {
