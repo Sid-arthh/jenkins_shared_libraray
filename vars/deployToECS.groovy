c20203c67e535477c9c119e705fce5246b098d22
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-08-29 11:04:53 +0530

    Update deployToECS.groovy

--- vars/deployToECS.groovy
+++ vars/deployToECS.groovy
@@ -2,5 +2,15 @@ def call(String imageUri) {
     def taskDefinition = ecsUtils(imageUri)
     echo "Generated Task Definition: ${taskDefinition}"
     sh "aws ecs register-task-definition --cli-input-json '${taskDefinition}'"
-    sh "aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_NAME --force-new-deployment"
+    // sh "aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_NAME --force-new-deployment"
+    def serviceExists = sh(script: "aws ecs describe-services --cluster $ECS_CLUSTER_NAME --services $ECS_SERVICE_NAME", returnStatus: true)
+
+    if (serviceExists == 0) {
+        // Update the existing service
+        sh "aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_NAME --force-new-deployment"
+    } else {
+        // Create a new service
+        sh "aws ecs create-service --cluster $ECS_CLUSTER_NAME --service-name $ECS_SERVICE_NAME --task-definition '${taskDefinition}'"
+    }
+
 }
