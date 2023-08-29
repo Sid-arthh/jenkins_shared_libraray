9bc75bc965b8120999286e406e5ce8f333f17aa1
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-08-29 11:56:52 +0530

    Update deployToECS.groovy

--- vars/deployToECS.groovy
+++ vars/deployToECS.groovy
@@ -4,7 +4,7 @@ def call(String imageUri) {
     sh "aws ecs register-task-definition --cli-input-json '${taskDefinition}' --region ${AWS_REGION}"
     // sh "aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_NAME --force-new-deployment"
     def serviceExists = sh(script: "aws ecs describe-services --cluster $ECS_CLUSTER_NAME --services $ECS_SERVICE_NAME --region ${AWS_REGION}", returnStatus: true)
-
+    echo ${serviceExists}
     if (serviceExists == 0) {
         // Update the existing service
         sh "aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_NAME  --region ${AWS_REGION} --force-new-deployment"
