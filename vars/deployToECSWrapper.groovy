946f3919a7e6a4c2afb0764b4e2b03fcf7f8534d
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-08-29 10:37:40 +0530

    Update deployToECSWrapper.groovy

--- vars/deployToECSWrapper.groovy
+++ vars/deployToECSWrapper.groovy
@@ -1,6 +1,7 @@
 def call() {
     withCredentials([[
         $class: 'AmazonWebServicesCredentialsBinding',
+        credentialsId: 'AWS CRED',
         accessKeyVariable: 'AWS_ACCESS_KEY_ID',
         secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
     ]]) {
