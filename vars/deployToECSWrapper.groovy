f980149fde578c786558f84ac3578afb7e815eb7
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-08-29 11:16:43 +0530

    Update deployToECSWrapper.groovy

--- vars/deployToECSWrapper.groovy
+++ vars/deployToECSWrapper.groovy
@@ -5,8 +5,8 @@ def call() {
         accessKeyVariable: 'AWS_ACCESS_KEY_ID',
         secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
     ]]) {
-        def ecrRepoName = 'your-ecr-repo-name'
-        def imageUri = "${ecrRepoName}:latest"
+        // def ecrRepoName = 'your-ecr-repo-name'
+        def imageUri = "public.ecr.aws/nginx/nginx:mainline-alpine"
         
         deployToECS(imageUri)
     }
