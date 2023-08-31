15fc82da56350f9a82b545de7324bd06e9c1cc72
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-08-31 15:16:12 +0530

    Update deployToECS.groovy

--- vars/deployToECS.groovy
+++ vars/deployToECS.groovy
@@ -32,7 +32,7 @@ def call(String imageUri) {
             sh "aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_NAME --task-definition '${taskDefinitionArn}' --desired-count '1' --network-configuration 'awsvpcConfiguration={subnets=[subnet-064d3272b4081aa26,subnet-026a4de3b32ce30d4],securityGroups=[sg-02a405b8bb4fd7b4f],assignPublicIp=ENABLED}' --enable-execute-command --region ${AWS_REGION} --force-new-deployment"
             }
 
-    echo "Extracted Status: $extractedStatus"
+    // echo "Extracted Status: $extractedStatus"
     // echo serviceExistsOutput
     //                     // Extract the value after '--services'
     // def match = serviceExistsOutput =~ /"status":\s*"([^"]*)"/
