e6a143b975fd6928a61292d665fb6e2e45a3abf1
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-08-31 10:14:15 +0530

    Update deployToECS.groovy

--- vars/deployToECS.groovy
+++ vars/deployToECS.groovy
@@ -8,12 +8,16 @@ def call(String imageUri) {
     ).trim()
     def serviceExists = sh(script: "aws ecs describe-services --cluster $ECS_CLUSTER_NAME --services $ECS_SERVICE_NAME --region ${AWS_REGION}", returnStatus: true)
     echo "Value of it is ${serviceExists}"
-    if (serviceExists != 0) {
+    if (serviceExists.failures) {
+        echo "Service is not there 
+        }
+    else echo "service is there"
+    // if (serviceExists != 0) {
 
-        sh "aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_NAME  --region ${AWS_REGION} --desired-count 1 --force-new-deployment"
-    } else {
+    //     sh "aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_NAME  --region ${AWS_REGION} --desired-count 1 --force-new-deployment"
+    // } else {
 
         sh "aws ecs create-service --cluster $ECS_CLUSTER_NAME --service-name $ECS_SERVICE_NAME --task-definition '${taskDefinitionArn}' --desired-count '1' --launch-type 'FARGATE' --network-configuration 'awsvpcConfiguration={subnets=[subnet-064d3272b4081aa26,subnet-026a4de3b32ce30d4],securityGroups=[sg-02a405b8bb4fd7b4f],assignPublicIp=ENABLED}' --enable-execute-command --region ${AWS_REGION}"
-    }
+    // }
 
 }
