280cc1fe428b229335ea0975f39141f8fbcf701c
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-08-31 13:27:40 +0530

    Update deployToECS.groovy

--- vars/deployToECS.groovy
+++ vars/deployToECS.groovy
@@ -12,10 +12,10 @@ def call(String imageUri) {
         ).trim()
     echo serviceExistsOutput
                         // Extract the value after '--services'
-    def match = serviceExistsOutput =~ /"services":\s*\[([^\]]*)\]/
+    def match = serviceExistsOutput =~ /"status":\s*\[([^\]]*)\]/
     echo "HHHH $match"
-    // def extractedServicesValue = match ? match[0][1] : "fall"
-    // echo "Extracted Service: $extractedServicesValue"
+    def extractedServicesValue = match ? match[0][1] : "fall"
+    echo "Extracted Service: $extractedServicesValue"
     // def serviceExists = new groovy.json.JsonSlurper().parseText(serviceExistsOutput)
     // def serviceExists = sh(script: "aws ecs describe-services --cluster $ECS_CLUSTER_NAME --services $ECS_SERVICE_NAME --region ${AWS_REGION}",returnStdout: true)
     // // echo "Returning ${serviceExists.services.status}"
@@ -36,7 +36,7 @@ def call(String imageUri) {
     // else echo "HELLO@"
     // if ("$serviceExists" != 0) {
     //     echo "OHH"
-    // sh "aws ecs create-service --cluster $ECS_CLUSTER_NAME --service-name $ECS_SERVICE_NAME --task-definition '${taskDefinitionArn}' --desired-count '1' --launch-type 'FARGATE' --network-configuration 'awsvpcConfiguration={subnets=[subnet-064d3272b4081aa26,subnet-026a4de3b32ce30d4],securityGroups=[sg-02a405b8bb4fd7b4f],assignPublicIp=ENABLED}' --enable-execute-command --region ${AWS_REGION}"
+    sh "aws ecs create-service --cluster $ECS_CLUSTER_NAME --service-name $ECS_SERVICE_NAME --task-definition '${taskDefinitionArn}' --desired-count '1' --launch-type 'FARGATE' --network-configuration 'awsvpcConfiguration={subnets=[subnet-064d3272b4081aa26,subnet-026a4de3b32ce30d4],securityGroups=[sg-02a405b8bb4fd7b4f],assignPublicIp=ENABLED}' --enable-execute-command --region ${AWS_REGION}"
 
     // sh "aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_NAME --task-definition '${taskDefinitionArn}' --desired-count '1' --network-configuration 'awsvpcConfiguration={subnets=[subnet-064d3272b4081aa26,subnet-026a4de3b32ce30d4],securityGroups=[sg-02a405b8bb4fd7b4f],assignPublicIp=ENABLED}' --enable-execute-command --region ${AWS_REGION} --force-new-deployment"
     // } else {
