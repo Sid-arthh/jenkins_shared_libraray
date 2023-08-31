e6837513e8b80bd20ebbc20b52ef48dfc26da8f2
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-08-31 11:12:25 +0530

    Update deployToECS.groovy

--- vars/deployToECS.groovy
+++ vars/deployToECS.groovy
@@ -11,9 +11,9 @@ def call(String imageUri) {
     returnStdout: true
         )
 
-    def serviceExists = new groovy.json.JsonSlurper().parseText(serviceExistsOutput)
+    // def serviceExists = new groovy.json.JsonSlurper().parseText(serviceExistsOutput)
     // def serviceExists = sh(script: "aws ecs describe-services --cluster $ECS_CLUSTER_NAME --services $ECS_SERVICE_NAME --region ${AWS_REGION}",returnStdout: true)
-    echo "Returning ${serviceExists.services.status}"
+    // echo "Returning ${serviceExists.services.status}"
     // if("$serviceExists.services.status"!='INACTIVE' || "$serviceExists".isEmpty())
     //     {
     //         sh "aws ecs create-service --cluster $ECS_CLUSTER_NAME --service-name $ECS_SERVICE_NAME --task-definition '${taskDefinitionArn}' --desired-count '1' --launch-type 'FARGATE' --network-configuration 'awsvpcConfiguration={subnets=[subnet-064d3272b4081aa26,subnet-026a4de3b32ce30d4],securityGroups=[sg-02a405b8bb4fd7b4f],assignPublicIp=ENABLED}' --enable-execute-command --region ${AWS_REGION}"
