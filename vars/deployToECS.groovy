606b29b73f6121d5d9e1dc97903d27f8e1d88e28
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-08-31 15:15:48 +0530

    Update deployToECS.groovy

--- vars/deployToECS.groovy
+++ vars/deployToECS.groovy
@@ -1,5 +1,6 @@
 def call(String imageUri) {
     def taskDefinition = ecsUtils(imageUri)
+     def extractedStatus ='null'
     echo "Generated Task Definition: ${taskDefinition}"
     // sh "aws ecs register-task-definition --cli-input-json '${taskDefinition}' --region ${AWS_REGION}"
     def taskDefinitionArn = sh(
@@ -17,25 +18,19 @@ def call(String imageUri) {
         int statusValueEnd = serviceExistsOutput.indexOf("\"", statusValueStart + 1)
         if (statusValueStart != -1 && statusValueEnd != -1) {
             
-            def extractedStatus = serviceExistsOutput.substring(statusValueStart + 1, statusValueEnd)
+            extractedStatus = serviceExistsOutput.substring(statusValueStart + 1, statusValueEnd)
             echo "IExtracted Status: $extractedStatus"
-            if("$extractedStatus"=='INACTIVE'){
-                sh "aws ecs create-service --cluster $ECS_CLUSTER_NAME --service-name $ECS_SERVICE_NAME --task-definition '${taskDefinitionArn}' --desired-count '1' --launch-type 'FARGATE' --network-configuration 'awsvpcConfiguration={subnets=[subnet-064d3272b4081aa26,subnet-026a4de3b32ce30d4],securityGroups=[sg-02a405b8bb4fd7b4f],assignPublicIp=ENABLED}' --enable-execute-command --region ${AWS_REGION}"
-            }
-            else {
-                
-                sh "aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_NAME --task-definition '${taskDefinitionArn}' --desired-count '1' --network-configuration 'awsvpcConfiguration={subnets=[subnet-064d3272b4081aa26,subnet-026a4de3b32ce30d4],securityGroups=[sg-02a405b8bb4fd7b4f],assignPublicIp=ENABLED}' --enable-execute-command --region ${AWS_REGION} --force-new-deployment"
-            }
             } 
         else {
                  echo "Unable to extract status"
              }
      } 
-    else {
-             echo "Status field not found"
-             sh "aws ecs create-service --cluster $ECS_CLUSTER_NAME --service-name $ECS_SERVICE_NAME --task-definition '${taskDefinitionArn}' --desired-count '1' --launch-type 'FARGATE' --network-configuration 'awsvpcConfiguration={subnets=[subnet-064d3272b4081aa26,subnet-026a4de3b32ce30d4],securityGroups=[sg-02a405b8bb4fd7b4f],assignPublicIp=ENABLED}' --enable-execute-command --region ${AWS_REGION}"
-
-                    }
+    if("$extractedStatus"=='INACTIVE' || statusIndex != -1){
+            sh "aws ecs create-service --cluster $ECS_CLUSTER_NAME --service-name $ECS_SERVICE_NAME --task-definition '${taskDefinitionArn}' --desired-count '1' --launch-type 'FARGATE' --network-configuration 'awsvpcConfiguration={subnets=[subnet-064d3272b4081aa26,subnet-026a4de3b32ce30d4],securityGroups=[sg-02a405b8bb4fd7b4f],assignPublicIp=ENABLED}' --enable-execute-command --region ${AWS_REGION}"
+            }
+    else {  
+            sh "aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_NAME --task-definition '${taskDefinitionArn}' --desired-count '1' --network-configuration 'awsvpcConfiguration={subnets=[subnet-064d3272b4081aa26,subnet-026a4de3b32ce30d4],securityGroups=[sg-02a405b8bb4fd7b4f],assignPublicIp=ENABLED}' --enable-execute-command --region ${AWS_REGION} --force-new-deployment"
+            }
 
     echo "Extracted Status: $extractedStatus"
     // echo serviceExistsOutput
