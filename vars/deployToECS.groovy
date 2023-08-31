fca4dca5b2821dbe790f45c4d43fbecefe102b71
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-08-31 10:51:01 +0530

    Update deployToECS.groovy

--- vars/deployToECS.groovy
+++ vars/deployToECS.groovy
@@ -14,7 +14,7 @@ def call(String imageUri) {
     def serviceExists = new groovy.json.JsonSlurper().parseText(serviceExistsOutput)
     // def serviceExists = sh(script: "aws ecs describe-services --cluster $ECS_CLUSTER_NAME --services $ECS_SERVICE_NAME --region ${AWS_REGION}",returnStdout: true)
     echo "Returning ${serviceExists.services.status}"
-    if($serviceExists.services.status!='INACTIVE')
+    if("$serviceExists.services.status"!='INACTIVE')
         {
             echo "HELLO"
         }
