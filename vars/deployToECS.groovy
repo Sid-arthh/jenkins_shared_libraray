11f22b25c91a3d22c5a81f177613ba71a7b4e89a
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-08-31 11:57:18 +0530

    Update deployToECS.groovy

--- vars/deployToECS.groovy
+++ vars/deployToECS.groovy
@@ -16,7 +16,7 @@ def call(String imageUri) {
     echo "Returning ${serviceExists.services.status}"
     // echo "yeyee $serviceExistsOutput ------------"
     def status = "${serviceExists.services.status}"
-    echo $status
+    echo status
     // if("$serviceExists.services.status"!='INACTIVE' || "$serviceExists".isEmpty())
     // //     {
     // //     sh(script: "aws ecs create-service --cluster $ECS_CLUSTER_NAME --service-name $ECS_SERVICE_NAME --task-definition '${taskDefinitionArn}' --desired-count '1' --launch-type 'FARGATE' --network-configuration 'awsvpcConfiguration={subnets=[subnet-064d3272b4081aa26,subnet-026a4de3b32ce30d4],securityGroups=[sg-02a405b8bb4fd7b4f],assignPublicIp=ENABLED}' --enable-execute-command --region ${AWS_REGION}")
