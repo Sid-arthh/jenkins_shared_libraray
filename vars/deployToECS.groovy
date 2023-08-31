ca6539a733589a6ced94a8eb2762957c110ab4b7
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-08-31 13:04:15 +0530

    Update deployToECS.groovy

--- vars/deployToECS.groovy
+++ vars/deployToECS.groovy
@@ -14,7 +14,7 @@ def call(String imageUri) {
     // def serviceExists = new groovy.json.JsonSlurper().parseText(serviceExistsOutput)
     // def serviceExists = sh(script: "aws ecs describe-services --cluster $ECS_CLUSTER_NAME --services $ECS_SERVICE_NAME --region ${AWS_REGION}",returnStdout: true)
     // echo "Returning ${serviceExists.services.status}"
-    def status=serviceExistsOutput[0].size()<1
+    def status=serviceExistsOutput['services'].size()<1
     echo "wow $serviceExistsOutput[0]"
     echo "LLLL"
     echo serviceExistsOutput
