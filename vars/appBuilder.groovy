22c65fc0a11826c8e4a606bb7932cd664f2dcf67
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-09-01 12:41:19 +0530

    Update appBuilder.groovy

--- vars/appBuilder.groovy
+++ vars/appBuilder.groovy
@@ -1,32 +1,32 @@
 def buildApp() {
-    stage('Build_App') {
-        steps {
-            script {
-                dir('app') {
-                    sh "mvn clean package"
-                }
-            }
-        }
-    }
+    // stage('Build_App') {
+    //     steps {
+    //         script {
+    //             dir('app') {
+            sh "mvn clean package"
+    //             }
+    //         }
+    //     }
+    // }
 }
 
 def DockerBuildPushToECR() {
-    stage('Building Image') {
-        steps {
-            script {
-                dir('app') {
-                    sh "docker build -t $ECR_REPO_NAME ."
-                }
-            }
-        }
+    // stage('Building Image') {
+    //     steps {
+    //         script {
+    //             dir('app') {
+            sh "docker build -t $ECR_REPO_NAME ."
+        //         }
+        //     }
+        // }
     }
     
-    script {
-        dir('app') {
+    // script {
+    //     dir('app') {
             sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/e5i0e5h0"
             sh "docker tag $ECR_REPO_NAME public.ecr.aws/e5i0e5h0/$ECR_REPO_NAME:latest"
             sh "docker push public.ecr.aws/e5i0e5h0/$ECR_REPO_NAME:latest"
-        }
-    }
-    return "public.ecr.aws/e5i0e5h0/$ECR_REPO_NAME:latest"
+    //     }
+    // }
+            return "public.ecr.aws/e5i0e5h0/$ECR_REPO_NAME:latest"
 }
