ba27e3f46b3b938dc1bbe7bf3735ff3d7483f651
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-09-01 15:38:51 +0530

    Update appBuilder.groovy

--- vars/appBuilder.groovy
+++ vars/appBuilder.groovy
@@ -16,26 +16,8 @@ def DockerBuildPushToECR() {
     // stage('Building Image') {
     //     steps {
     script {
-     dir('app') {
-        withCredentials([[
-        $class: 'AmazonWebServicesCredentialsBinding',
-        credentialsId: 'AWS CRED',
-        accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-        secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-    ]]) {
-        // def ecrRepoName = 'your-ecr-repo-name'
-        
-        
-    
             sh "ls"
             sh "docker build -t $ECR_REPO_NAME ."
-        //         }
-        //     }
-        // }
-    // }
-    
-    // script {
-    //     dir('app') {
             sh "aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/e5i0e5h0"
             sh "docker tag $ECR_REPO_NAME:latest public.ecr.aws/e5i0e5h0/$ECR_REPO_NAME:latest"
             sh "docker push public.ecr.aws/e5i0e5h0/$ECR_REPO_NAME:latest"
