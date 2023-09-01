e56eb888bab392bf28b8352dfa1cc58881e0009d
Sid-arthh <96643963+Sid-arthh@users.noreply.github.com>
2023-09-01 16:39:48 +0530

    Update appBuilder.groovy

--- vars/appBuilder.groovy
+++ vars/appBuilder.groovy
@@ -1,26 +1,17 @@
 def buildApp() {
-    // stage('Build_App') {
-    //     steps {
             script {
-    
                 sh "ls"
                 sh "mvn clean package"
             }
-    //     }
-    // }
 }
 
 def DockerBuildPushToECR() {
-    // stage('Building Image') {
-    //     steps {
     script {
             sh "ls"
             sh "docker build -t $ECR_REPO_NAME ."
             sh "aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/e5i0e5h0"
             sh "docker tag $ECR_REPO_NAME:latest public.ecr.aws/e5i0e5h0/$ECR_REPO_NAME:latest"
             sh "docker push public.ecr.aws/e5i0e5h0/$ECR_REPO_NAME:latest"
-    //     }
-    // }
             return "public.ecr.aws/e5i0e5h0/$ECR_REPO_NAME:latest"
 }
 }
